8332ab5c95fd4e3349634c33eced9a9ec69fc8c7
Alexander Forselius <drsounds@gmail.com>
2013-07-10 15:58:25 +0200

    Fixed some stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 require([
   'strings/main.lang',
+  '$api/models',
   'scripts/qizone#Qizone',
-  '$views/tabbar#TabBar'
-
-
-], function(mainStrings, Qizone, TabBar) {
+  '$views/tabbar#TabBar',
+  'scripts/jquery-1.10.2.min',
+  'scripts/bootstrap'
+], function(mainStrings, models, Qizone, TabBar, bootstrap) {
   'use strict';
   var tabBar = TabBar.withTabs([
     {id: 'overview', name: 'Overview', active: true},
@@ -12,4 +13,31 @@ require([
     {id: 'artists', name: 'Artists'}
   ]);
   tabBar.addToDom(document.body, 'prepend');
+
+  function navigate(args) {
+    console.log("navigating", args);
+    var page = args > 0 ? args[args.length-1] : "overview";
+    setSection(page);
+  }
+  setSection("overview");
+  models.application.addEventListener('arguments', function (app) {
+    console.log(arguments);
+    navigate(arguments);
+  });
+  function setSection(id) {
+    var sections = document.querySelectorAll('section');
+    for(var i = 0; i < sections.length; i++) {
+      var section = sections[i];
+      section.style.display = section.id == id ? "block" : "none";
+    }
+  }
+
+  navigate(['overview']);
+
+  var qizone = Qizone.fromURI("spotify:qizone:localhost:qizone:ws");
+  console.log(qizone);
+  qizone.load('playlists', 'artists', 'features', 'releases').done(function (qizone) {
+    alert("A");
+    console.log("I'm here", qizone);
+  }).always(function (qizone) {console.log("A")});
 });
--- scripts/qizone.js
+++ scripts/qizone.js
@@ -1,57 +1,96 @@
 require([
   'strings/main.lang',
-  '$api/models'
-], function(mainStrings, models) {
+  '$api/models',
+  'scripts/bootstrap'
+], function(mainStrings, models, bootstrap) {
   'use strict';
- 	function Qizone () {
+
+  
+ 	function Qizone (uri) {
  		var self = this;
+    self.uri = uri.split(':');
+    self.domain = self.uri.slice(2);
  		this._resolve = function (entity, callback) {
+      var xmlHttp = new XMLHttpRequest();
  			xmlHttp.onreadystatechange = function () {
 	 			if(xmlHttp.readyState == 4) {
+          console.log(xmlHttp.status);
 	 				if(xmlHttp.status == 200) {
 	 					var data = JSON.parse(xmlHttp.responseText);
-						self.resolve(entity, data['entity']);
+						
+            console.log(data);
 						callback.success(data);
 	 				} else {
 	 					callback.fail();
 	 				}
 	 				callback.always(data);
-	 			}
+	 			} else {
+
+        }
 	 		};
-	 		xmlHttp.open('GET', 'http://localhost/qizone/ws/' + entity + 's', true);
-	 		xmlHTtp.send(null);
+      var uri ='http://' + self.domain.join('/') + '/' + entity + 's';
+	 		xmlHttp.open('GET', uri, true);
+	 		xmlHttp.send(null);
+      console.log("T");
 		};
+    this._load = function () {
+      console.log(self);
+      self._resolve('playlist', {
+        success: function (playlists) {
+          self.resolve('playlists', playlists.playlists);
+        },
+        always: function () {
+          self._resolve('release', {
+            success: function (releases) {
+
+             console.log("A");
+              var albums = [];
+              console.log("Releases", releases);
+              for(var i = 0; i < releases.releases.length; i++) {
+                albums.push(releases.releases[i].uri);
+              }
+              albums = models.Album.fromURIs(albums);
+              self.resolve('releases', albums);
+              console.log(albums);
+            },
+            always: function () {
+
+              self._resolve('feature', {
+                success: function (features) {
+
+                  self.resolve('features', features);
+                },
+                always: function () {
+                  self._resolve('artist', {
+                    success: function (artists) {
+                      self.resolve('artists', artists);
+                    },
+                    always: function () {
+                     self.resolveDone();
+
+                    }
+
+                  });
+                  console.log("ffafa");
+                }
+              });
+            }
+          });
+          
+        }
+      });
+    };
  	
- 	}
+                 
+ 	};
+  exports.Qizone = Qizone;
+  exports.Qizone.fromURI = function (uri) {
+    return new exports.Qizone(uri);
+  }
 
  	SP.inherit(Qizone, models.Loadable);
- 	Qizone.prototype._load = function () {
- 		
- 		self.resolve('playlist', {
- 			success: function (playlists) {
- 				self.resolve('playlists', playlists);
- 			},
- 			always: function () {
- 				self._resolve('artist', {
- 					success: function (artists) {
- 						self.resolve('artists', artists);
- 					},
- 					always: function () {
- 						self._resolve('feature', {
- 							success: function (features) {
- 								self.resolve('features', features);
- 							},
- 							always: function () {
- 								self.resolveDone();
- 							}
- 						});
- 					}
- 				});
- 			}
- 		});
- 		
- 	};
- 	models.Loadable.define(Qizone, ['playlists', 'artists', 'features', 'releases'], '_load');
+
+ 	models.Loadable.define(exports.Qizone, ['playlists', 'artists', 'features', 'releases'], '_load');
 
  	exports.Qizone = Qizone;
 });
